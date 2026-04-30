e7004fb9c05332687d324bd7774e13465422a6f0
Anubis1753 <anubis1753@gmail.com>
2026-04-30 09:02:53 -0600

    sandbox2

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        // This is a C# comment
         Console.WriteLine("Hello Sandbox World!");
+        // bool yes = false;
+        for (int i = 0; i < 10; i ++)
+        {
+            Console.WriteLine($"Something {i}");
+        }
     }
+    
 }
