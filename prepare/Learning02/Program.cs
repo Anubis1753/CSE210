08e07f5c85e2fa17748f9093b63ac64ca1fcac55
Anubis1753 <anubis1753@gmail.com>
2026-05-04 12:20:28 -0600

    did the assignment prep 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        job job1 = new job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 2002;
+        job1._endYear = 2025;
+
+        job job2 = new job();
+        job2._jobTitle = "Team Lead";
+        job2._company = "Adobe";
+        job2._startYear = 2005;
+        job2._endYear = 2014;
+
+        resume resume1 = new resume();
+        resume1._name = "John";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+
     }
 }
