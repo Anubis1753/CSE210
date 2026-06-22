1a270d3bc44216130c19260ee12aa45422d37808
Anubis1753 <anubis1753@gmail.com>
2026-06-22 12:14:31 -0600

    Polymorphism prepaire

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,50 +1,28 @@
 using System;
+using System.Globalization;
 using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Fraction f1 = new Fraction();
-        Fraction f2 = new Fraction(6);
-        Fraction f3 = new Fraction(6 , 7);
+        List<Shape> shapes = new List<Shape>();
 
-        Console.WriteLine(f1.GetFractionString());
-        Console.WriteLine(f1.GetDecimal());
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
 
-        Console.WriteLine(f2.GetFractionString());
-        Console.WriteLine(f2.GetDecimal());
+        Rectangle s2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(s2);
 
-        Console.WriteLine(f3.GetFractionString());
-        Console.WriteLine(f3.GetDecimal());
+        Circle s3 = new Circle("Green", 6);
+        shapes.Add(s3);
 
-        Fraction f4 = new Fraction();
-        f4.SetTop(5);
-        f4.SetBottom(1);
-        Console.WriteLine(f4.GetFractionString());
-        Console.WriteLine(f4.GetDecimal());
-
-        Fraction f5 = new Fraction(2, 3);
-        Console.WriteLine(f5.GetFractionString());
-        Console.WriteLine(f5.GetDecimal());
-
-        Fraction f6 = new Fraction(1, 3);
-        Console.WriteLine(f6.GetFractionString());
-        Console.WriteLine(f6.GetDecimal());
-
-        Fraction f7 = new Fraction();
-        Random random = new Random(); 
-        for (int i = 1; i <= 20; i++)
+        foreach(Shape s in shapes)
         {
-            int ranTop = random.Next(1, 101);
-            int ranBottom = random.Next(1,101);
+            string color = s.GetColor();
+            double area = s.GetArea();
 
-            f7.SetTop(ranTop);
-            f7.SetBottom(ranBottom);
-
-            Console.Write($"Fraction {i + 1}: ");
-            Console.Write($"string: {f5.GetFractionString()}");
-            Console.WriteLine($" Number: {f5.GetDecimal()}");
+            Console.WriteLine($"The {color} shape has an area of {area}.");
         }
     }
 }
