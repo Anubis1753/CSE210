8607c63a72b3cc241b2d3a8ced0f8d10f046fa08
Anubis1753 <anubis1753@gmail.com>
2026-07-18 17:07:30 -0600

    updated activity to make everything display two decimal places

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -20,6 +20,6 @@ public abstract class Activity
 
     public string GetSummary()
     {
-        return $"{_date} {this.GetType().Name} {_minutes} minutes - " + $"Distance {GetDistance():0.0} miles, " + $"Speed {GetSpeed():0.0} mph, " + $"Pace {GetPace()} min per mile";
+        return $"{_date} {this.GetType().Name} {_minutes} minutes - " + $"Distance {GetDistance():0.00} miles, " + $"Speed {GetSpeed():0.00} mph, " + $"Pace {GetPace():0.00} min per mile";
     }
 }
