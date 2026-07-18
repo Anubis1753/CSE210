6256f634b09a677db5583c2564869a4f7adba85d
Anubis1753 <anubis1753@gmail.com>
2026-07-18 17:03:20 -0600

    foundation 4 update

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,13 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Running RunningActivity = new Running("03 Nov 2022", 30, 3.0);
-        Cycling CyclingActivity = new Cycling("04 Nov 2022", 45, 15.0);
-        Swimming SwimmingActivity = new Swimming("05 Nov 2022", 20, 40);
+        Running R1 = new Running("03 Nov 2022", 30, 3.0);
+        Cycling C1 = new Cycling("04 Nov 2022", 45, 15.0);
+        Swimming S1 = new Swimming("05 Nov 2022", 20, 40);
 
         List<Activity> activities = new List<Activity>
         {
-            RunningActivity, CyclingActivity, SwimmingActivity
+            R1, C1, S1
         };
 
         foreach (Activity activity in activities)
